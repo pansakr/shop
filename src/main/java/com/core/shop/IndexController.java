93f00c18202302fc7443f4944d379bfde3e9b08e
JaeHyeon <nitsss15@gmail.com>
2023-08-08 15:22:44 +0900

    "index Controller modify"

--- src/main/java/com/core/shop/IndexController.java
+++ src/main/java/com/core/shop/IndexController.java
@@ -12,14 +12,6 @@ public class IndexController {
     @GetMapping("/")
     public String index(Authentication authentication, Model model){
 
-        // 유저정보가 있으면 실행
-//        if(authentication.getPrincipal() != null) {
-//            Object principal = authentication.getPrincipal();
-//            UserDetails userDetails = (UserDetails) principal;
-//            String username = userDetails.getUsername();
-//            model.addAttribute("member", username);
-//        }
-
         return "index";
     }
 }
